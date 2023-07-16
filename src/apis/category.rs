eeee2cbef69ee8c227e2a0e3f9952fad27139450
deVOID <dirtymbgamer@gmail.com>
2023-07-16 16:43:18 +0330

    Add delete category api

--- src/apis/category.rs
+++ src/apis/category.rs
@@ -4,7 +4,7 @@ use crate::models::category::{
 };
 use crate::prelude::*;
 use crate::serializers::category::CreateCategory;
-use actix_web::{get, post, web, HttpResponse};
+use actix_web::{delete, get, patch, post, put, web, HttpResponse};
 use sea_orm::{entity::*, prelude::*, query::*, ActiveValue::Set, DatabaseBackend, EntityTrait};
 use serde_json::json;
 use slugify::slugify;
@@ -23,6 +23,20 @@ pub async fn get_list_categories_api(db: web::Data<AppState>) -> ResponseResult 
         .await?;
 
     Ok(HttpResponse::Ok().json(json!({ "ok": categories })))
+}
+
+#[delete("/category/{id}/")]
+pub async fn delete_category_api(path: web::Path<i32>, db: web::Data<AppState>) -> ResponseResult {
+    Category::delete_by_id(path.into_inner())
+        .exec(&db.conn)
+        .await?;
+
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[put("/category/{id}/")]
+pub async fn update_category_api() -> ResponseResult {
+    todo!()
 }
 
 #[post("/category/create/")]
